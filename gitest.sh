8f02990122347801709d032484301e9ce71b2e2c
Vasiliki - Mantzana-Oikonomaki <58005239+BillieMantzOik@users.noreply.github.com>
2021-01-20 14:38:11 +0100

    Create gitest.sh

---
+++ gitest.sh
@@ -0,0 +1,8 @@
+#! bin/bash
+mkdir WEEK2_DAY3_EXERCISE
+cd ~/WEEK2_DAY3_EXERCISE
+mkdir DATA_REPOSITORT
+touch ~/WEEK2_DAY3_EXERCISE/DATA_REPOSITORY/initial_file.txt
+nano ~/WEEK2_DAY3_EXERCISE/DATA_REPOSITORY/initial_file.txt
+cd ~/WEEK2_DAY3_EXERCISE/DATA_REPOSITORY
+echo "Vasiliki Mantzana Oikonomaki" > initial_file.txt
